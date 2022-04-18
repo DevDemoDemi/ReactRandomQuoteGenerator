7888cfcf57ace59c305254ab9e7316a0ccbb108d
Demi Parrilla <demiparrilla802@gmail.com>
2022-04-18 12:53:58 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import QuoteCompo from "./components/quote";
+import './style.css'
+
+function App() {
+  return (
+    <div className='style'>
+      <QuoteCompo />
+    </div>
+  );
+}
+
+export default App;
